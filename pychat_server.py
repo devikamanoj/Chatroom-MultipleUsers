15fca21e985e72780463a6b28bba0c0dafca4fe9
lakshmi-warrier <lakshmi0105warrier@gmail.com>
2022-06-08 09:35:23 +0000

    comments added

--- pychat_server.py
+++ pychat_server.py
@@ -6,31 +6,47 @@ import pychat_util
 
 READ_BUFFER = 4096
 
-host = sys.argv[1] if len(sys.argv) >= 2 else ''
-listen_sock = pychat_util.create_socket((host, pychat_util.PORT))
+ip_address = sys.argv[1] if len(sys.argv) >= 2 else ''
+#sys.argv to read input from command line
+#python3 pychat_server.py ipadd returns sys.argv as [pychat_server.py, ipadd] => len = 2
+#ip_address = 1st element
 
+#creates socket to listen to the port using the 'ip_address'
+listen_sock = pychat_util.create_socket((ip_address, pychat_util.PORT))
+
+#creates a Hall object
 hall = Hall()
+
+#adds the created socket to the connected sockets list
 connection_list = []
 connection_list.append(listen_sock)
 
 while True:
-    # User.fileno()
-    read_users, write_users, error_sockets = select.select(connection_list, [], [])
-    for player in read_users:
-        if player is listen_sock: # new connection, player is a socket
-            new_socket, add = player.accept()
-            new_player = User(new_socket)
-            connection_list.append(new_player)
-            hall.welcome_new(new_player)
+    # every user is a socket
+    # select finds the status of file descriptors - read, write and error
+    # --------------- doubt ------------
 
-        else: # new message
-            msg = player.socket.recv(READ_BUFFER)
+    read_users, write_users, error_sockets = select.select(connection_list, [], [])
+    for user in read_users:
+        if user is listen_sock: 
+            # new user
+            new_socket, add = user.accept()
+
+            # create a new socket, ie user
+            new_user = User(new_socket)
+            # add the new user to the connection list
+            connection_list.append(new_user)
+            # print welcome msg
+            hall.welcome_new(new_user)
+
+        else:
+            msg = user.socket.recv(READ_BUFFER)
             if msg:
                 msg = msg.decode().lower()
-                hall.handle_msg(player, msg)
+                hall.handle_msg(user, msg)
             else:
-                player.socket.close()
-                connection_list.remove(player)
+                user.socket.close()
+                connection_list.remove(user)
 
     for sock in error_sockets: # close error sockets
         sock.close()
--- pychat_util.py
+++ pychat_util.py
@@ -1,8 +1,6 @@
 # implementing 3-tier structure: Hall --> Room --> Clients;
 from termcolor import colored
-
 import socket
-import pdb
 
 MAX_CLIENTS = 30
 PORT = 22222
@@ -10,10 +8,15 @@ QUIT_STRING = '<$quit$>'
 
 
 def create_socket(address):
+    # AF_INET: address family of internet
+    # SOCK_STREAM: by def: TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # allows to resuse address
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setblocking(0)
+    # binds address to socket
     s.bind(address)
+    # listens to a max of 30 clients
     s.listen(MAX_CLIENTS)
     print(colored(f"Now listening at {(str(address))}", 'blue'))
     return s
@@ -25,7 +28,7 @@ class Hall:
         self.rooms = {}
 
         # dictionary having user_name and the room_name as k:v pair - to see to which room the user belongs to
-        self.room_user_map = {}
+        self.user_room_map = {}
 
     def welcome_new(self, new_user):
         new_user.socket.sendall(
@@ -39,6 +42,7 @@ class Hall:
             user.socket.sendall(msg.encode())
         else:
             msg = 'Active rooms:\n'
+            # print details and users of every room
             for room in self.rooms:
                 msg += room + ": " + \
                     str(len(self.rooms[room].users)) + " user(s)\n"
@@ -55,9 +59,10 @@ class Hall:
             + b'\n'
 
         print(colored(user.name, 'yellow') + " > " + msg)
+
+        # new connection
         if "name:" in msg:
-            name = msg.split()[1]
-            user.name = name
+            user.name = msg.split()[1]
             print(colored(f"New connection from: {user.name}", 'green'))
             user.socket.sendall(instructions)
 
@@ -65,21 +70,35 @@ class Hall:
             same_room = False
             if len(msg.split()) >= 2:  # error check
                 room_name = msg.split()[1]
-                if user.name in self.room_user_map:  # switching?
-                    if self.room_user_map[user.name] == room_name:
+
+                # if user in the dict already
+                if user.name in self.user_room_map:
+                    # if the input room is same as the room in which user is there,
+                    if self.user_room_map[user.name] == room_name:
                         user.socket.sendall(
                             b'You are already in room: ' + room_name.encode())
                         same_room = True
                     else:  # switch
-                        old_room = self.room_user_map[user.name]
+                        old_room = self.user_room_map[user.name]
+
+                        # removes the user from the old room
                         self.rooms[old_room].remove_user(user)
+                
                 if not same_room:
-                    if not room_name in self.rooms:  # new room:
+                    # if the room does not exist yet, create a new Room object
+                    if not room_name in self.rooms:
                         new_room = Room(room_name)
+                        # assign the ROom object to the room_name in the dict
                         self.rooms[room_name] = new_room
+
+                    # add the user to the room
                     self.rooms[room_name].users.append(user)
+
+                    # welcomes user
                     self.rooms[room_name].welcome_new(user)
-                    self.room_user_map[user.name] = room_name
+
+                    # assigns this user to the new room
+                    self.user_room_map[user.name] = room_name
             else:
                 user.socket.sendall(instructions)
 
@@ -95,29 +114,33 @@ class Hall:
 
         else:
             # check if in a room or not first
-            if user.name in self.room_user_map:
-                self.rooms[self.room_user_map[user.name]
+            if user.name in self.user_room_map:
+                self.rooms[self.user_room_map[user.name]
                            ].broadcast(user, msg.encode())
             else:
+                # a person can chat publicly without joining a room
                 msg = 'Join a room to chat \n' \
                     + 'Use [!show] to see available rooms! \n' \
                     + 'Use [!join room_name] to join a room! \n'
                 user.socket.sendall(msg.encode())
 
     def remove_user(self, user):
-        if user.name in self.room_user_map:
-            self.rooms[self.room_user_map[user.name]].remove_user(user)
-            del self.room_user_map[user.name]
+        if user.name in self.user_room_map:
+            self.rooms[self.user_room_map[user.name]].remove_user(user)
+            del self.user_room_map[user.name]
         print(colored(f'{user.name} has left the chat', 'red'))
 
 
 class Room:
     def __init__(self, name):
         self.users = []  # a list of sockets (i.e., users)
-        self.name = name
+        self.name = name  # room name
 
     def broadcast(self, from_user, msg):
-        msg = from_user.name.encode() + b":" + msg
+        # prints( user: msg)
+        msg = from_user.name.encode() + b" : " + msg
+
+        # sends the msg to every one in the room
         for user in self.users:
             user.socket.sendall(msg)
 
@@ -133,10 +156,12 @@ class Room:
 
 
 class User:
+    # gives name as "new" by default
     def __init__(self, socket, name="new"):
         socket.setblocking(0)
         self.socket = socket
         self.name = name
 
+    # returns file descriptor
     def fileno(self):
         return self.socket.fileno()
